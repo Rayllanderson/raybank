7c390399f45e2c0be1e8f1be14f0a7ac4dd388d2
Rayllanderson <rayllanderson@gmail.com>
2021-05-28 20:05:24 -0300

    Criando BottomNavigationBar

--- mobile/lib/src/views/home_page.dart
+++ mobile/lib/src/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/src/components/navigations/bottom_navigation.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -8,12 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Tela de início',
+    ),
+    Text(
+      'Tela de transferência',
+    ),
+    Text(
+      'Tela de depósito',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HOME PAGE'),
-      ),
+        appBar: AppBar(
+          title: const Text('Raybank'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigator(
+          selectedIndex: _selectedIndex,
+          onItemTapped: (index) {
+            _onItemTapped(index);
+          },
+        )
     );
   }
 }
