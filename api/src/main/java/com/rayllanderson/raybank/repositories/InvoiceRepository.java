0085ed08a786dec50003059d5ad93e3bad1ff315
Rayllanderson <rayllanderson@gmail.com>
2023-09-04 21:05:38 -0300

    criando repo para invoice

---
+++ api/src/main/java/com/rayllanderson/raybank/repositories/InvoiceRepository.java
@@ -0,0 +1,13 @@
+package com.rayllanderson.raybank.repositories;
+
+import com.rayllanderson.raybank.models.Invoice;
+import com.rayllanderson.raybank.models.InvoiceStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, String> {
+    List<Invoice> findAllByClosingDateAndStatus(LocalDate closingDate, InvoiceStatus status);
+    List<Invoice> findAllByDueDateAndStatus(LocalDate dueDate, InvoiceStatus status);
+}
