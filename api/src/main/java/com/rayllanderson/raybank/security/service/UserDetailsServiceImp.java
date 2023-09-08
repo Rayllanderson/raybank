2e582c065f2a36738b7e575ec69fcbf289e944b4
Rayllanderson <rayllanderson@gmail.com>
2023-09-08 20:27:55 -0300

    retirando suporte a jwt do projeto

--- api/src/main/java/com/rayllanderson/raybank/security/service/UserDetailsServiceImp.java
+++ api/src/main/java/com/rayllanderson/raybank/security/service/UserDetailsServiceImp.java
@@ -1,20 +1,18 @@
 package com.rayllanderson.raybank.security.service;
 
+import com.rayllanderson.raybank.models.User;
 import com.rayllanderson.raybank.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service("userDetailsService")
-public class UserDetailsServiceImp implements UserDetailsService {
+public class UserDetailsServiceImp {
 
     private final UserRepository repository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
     }
 
