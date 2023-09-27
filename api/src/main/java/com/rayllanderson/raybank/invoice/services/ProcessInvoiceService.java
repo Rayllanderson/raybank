9ef700d2d0b0532418db199abb2b3a96ee0e404b
Rayllanderson <rayllanderson@gmail.com>
2023-09-27 19:57:13 -0300

    refactor metodo processa invoice e ajuste teste unitario

--- api/src/main/java/com/rayllanderson/raybank/invoice/services/ProcessInvoiceService.java
+++ api/src/main/java/com/rayllanderson/raybank/invoice/services/ProcessInvoiceService.java
@@ -1,8 +1,6 @@
 package com.rayllanderson.raybank.invoice.services;
 
-import com.rayllanderson.raybank.card.models.Card;
-import com.rayllanderson.raybank.card.repository.CardRepository;
-import com.rayllanderson.raybank.exceptions.NotFoundException;
+import com.rayllanderson.raybank.invoice.gateway.InvoiceGateway;
 import com.rayllanderson.raybank.invoice.helper.InvoiceListHelper;
 import com.rayllanderson.raybank.invoice.models.Invoice;
 import com.rayllanderson.raybank.invoice.repository.InvoiceRepository;
@@ -26,41 +24,36 @@ import static com.rayllanderson.raybank.utils.InstallmentUtil.createDescription;
 @RequiredArgsConstructor
 public class ProcessInvoiceService {
 
+    private final InvoiceGateway invoiceGateway;
     private final InvoiceRepository invoiceRepository;
-    private final CardRepository cardRepository;
 
-    public List<Invoice> processInvoice(BigDecimal total, int installments, String paymentDescription, LocalDateTime ocurredOn, String cardId) {
+    public List<Invoice> processInvoice(BigDecimal total, Integer installmentsCount, String paymentDescription, LocalDateTime ocurredOn, String cardId) {
+        final int installments = installmentsCount == null || installmentsCount == 0 ? 1 : installmentsCount;
         final Set<Invoice> invoices = new HashSet<>(invoiceRepository.findAllByCard_Id(cardId));
 
         final Integer dayOfDueDate = getDayOfDueDate(cardId);
-
         final InvoiceListHelper invoiceList = new InvoiceListHelper(dayOfDueDate, invoices);
 
         final Invoice currentInvoice = invoiceList.getCurrentOpenInvoice().orElse(Invoice.createOpenInvoice(plusOneMonthOf(dayOfDueDate), cardId));
         checkOcurredDateItsOnRange(currentInvoice, ocurredOn.toLocalDate());
 
         final var installmentValue = calculateInstallmentValue(total, installments);
-        currentInvoice.processPayment(createDescription(paymentDescription, 1, installments), total, installmentValue, ocurredOn);
-
-        invoiceRepository.save(currentInvoice);
-        invoiceList.add(currentInvoice);
 
         Invoice invoiceCopy = currentInvoice;
-
-        for (int i = 1; i < installments; i++) {
+        for (int i = 0; i < installments; i++) {
+            invoiceCopy.processPayment(createDescription(paymentDescription, i + 1, installments), total, installmentValue, ocurredOn);
+            invoiceList.add(invoiceCopy);
             final var nextInvoice = invoiceList.getNextOf(invoiceCopy);
             invoiceCopy = nextInvoice.orElse(Invoice.create(invoiceList.getNextInvoiceDate(invoiceCopy), cardId));
-            invoiceCopy.processPayment(createDescription(paymentDescription, i + 1, installments), total, installmentValue, ocurredOn);
-            invoiceRepository.save(invoiceCopy);
-            invoiceList.add(invoiceCopy);
         }
+
+        invoiceRepository.saveAll(invoiceList.getInvoices());
+
         return Collections.unmodifiableList(invoiceList.getSortedInvoices());
     }
 
     private Integer getDayOfDueDate(String cardId) {
-        final Card card = cardRepository.findById(cardId)
-                .orElseThrow(() -> new NotFoundException(String.format("Card %s was not found", cardId)));
-        return card.getDayOfDueDate();
+        return invoiceGateway.getDayOfDueDateByCardId(cardId);
     }
 
     private static void checkOcurredDateItsOnRange(Invoice currentInvoice, LocalDate ocurredOn) {
@@ -70,7 +63,7 @@ public class ProcessInvoiceService {
 
         final LocalDate oldClosingDate = currentInvoice.getClosingDate().minusMonths(1);
         if (isAfterOrEquals(oldClosingDate, ocurredOn)) {
-            throw new IllegalArgumentException("'ocurredOn' must not be before current invoice closing date");
+            throw new IllegalArgumentException("'ocurredOn' must not be before 'current invoice' closing date - " + currentInvoice.getClosingDate());
         }
     }
 }
--- api/src/test/java/com/rayllanderson/raybank/invoice/services/ProcessInvoiceServiceTest.java
+++ api/src/test/java/com/rayllanderson/raybank/invoice/services/ProcessInvoiceServiceTest.java
@@ -1,8 +1,8 @@
 package com.rayllanderson.raybank.invoice.services;
 
-import com.rayllanderson.raybank.card.models.Card;
-import com.rayllanderson.raybank.card.repository.CardRepository;
+import com.rayllanderson.raybank.invoice.gateway.InvoiceGateway;
 import com.rayllanderson.raybank.invoice.repository.InvoiceRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,12 +14,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static com.rayllanderson.raybank.invoice.InvoiceUtils.bigDecimalOf;
 import static com.rayllanderson.raybank.invoice.InvoiceUtils.create;
 import static com.rayllanderson.raybank.invoice.InvoiceUtils.installment;
-import static com.rayllanderson.raybank.invoice.InvoiceUtils.parse;
 import static com.rayllanderson.raybank.invoice.models.InvoiceStatus.NONE;
 import static com.rayllanderson.raybank.invoice.models.InvoiceStatus.OPEN;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,14 +30,17 @@ class ProcessInvoiceServiceTest {
     @Mock
     private InvoiceRepository invoiceRepository;
     @Mock
-    private CardRepository cardRepository;
+    private InvoiceGateway invoiceGateway;
     @InjectMocks
     private ProcessInvoiceService processInvoiceService;
 
+    @BeforeEach
+    void setup() {
+        when(invoiceGateway.getDayOfDueDateByCardId("cId")).thenReturn(1);
+    }
 
     @Test
     void shouldProcessInvoices() {
-        when(cardRepository.findById("cId")).thenReturn(Optional.of(Card.builder().dayOfDueDate(1).build()));
         when(invoiceRepository.findAllByCard_Id("cId")).thenReturn(Collections.emptyList());
 
         final var invoices = processInvoiceService.processInvoice(BigDecimal.valueOf(150),
@@ -71,33 +72,28 @@ class ProcessInvoiceServiceTest {
     @Test
     void shouldReuseExistingInvoices() {
 
+        LocalDate dueDate = LocalDate.now().plusDays(10);
         final var savedInvoices = List.of(
-                create(parse("2023-10-02"), bigDecimalOf(50.00), OPEN, installment("alibaba 1/3", bigDecimalOf(150.00), bigDecimalOf(50.00))),
-                create(parse("2023-11-01"), bigDecimalOf(50.00), NONE, installment("alibaba 2/3", bigDecimalOf(150.00), bigDecimalOf(50.00))),
-                create(parse("2023-12-01"), bigDecimalOf(50.00), NONE, installment("alibaba 3/3", bigDecimalOf(150.00), bigDecimalOf(50.00)))
+                create(dueDate, bigDecimalOf(50.00), OPEN, installment("alibaba 1/3", bigDecimalOf(150.00), bigDecimalOf(50.00))),
+                create(dueDate.plusMonths(1), bigDecimalOf(50.00), NONE, installment("alibaba 2/3", bigDecimalOf(150.00), bigDecimalOf(50.00))),
+                create(dueDate.plusMonths(2), bigDecimalOf(50.00), NONE, installment("alibaba 3/3", bigDecimalOf(150.00), bigDecimalOf(50.00)))
         );
-        when(cardRepository.findById("cId")).thenReturn(Optional.of(Card.builder().dayOfDueDate(1).build()));
+        when(invoiceGateway.getDayOfDueDateByCardId("cId")).thenReturn(1);
         when(invoiceRepository.findAllByCard_Id("cId")).thenReturn(savedInvoices);
 
-        final var invoices = processInvoiceService.processInvoice(BigDecimal.valueOf(100), 2, "amazon", LocalDateTime.parse("2023-09-01T20:41:37"), "cId");
+        final var invoices = processInvoiceService.processInvoice(BigDecimal.valueOf(100), 2, "amazon", LocalDateTime.now(), "cId");
 
         final var invoice1 = invoices.get(0);
-        assertThat(invoice1.getClosingDate()).isEqualTo(LocalDate.parse("2023-09-26"));
-        assertThat(invoice1.getDueDate()).isEqualTo(LocalDate.parse("2023-10-02"));
         assertThat(invoice1.getTotal()).isEqualTo(new BigDecimal("100.00"));
         assertThat(invoice1.getInstallments()).isNotEmpty().hasSize(2);
         assertThat(invoice1.getInstallments().get(0).getDescription()).isEqualTo("alibaba 1/3");
         assertThat(invoice1.getInstallments().get(1).getDescription()).isEqualTo("amazon 1/2");
         final var invoice2 = invoices.get(1);
-        assertThat(invoice2.getClosingDate()).isEqualTo(LocalDate.parse("2023-10-26"));
-        assertThat(invoice2.getDueDate()).isEqualTo(LocalDate.parse("2023-11-01"));
         assertThat(invoice2.getTotal()).isEqualTo(new BigDecimal("100.00"));
         assertThat(invoice2.getInstallments()).isNotEmpty().hasSize(2);
         assertThat(invoice2.getInstallments().get(0).getDescription()).isEqualTo("alibaba 2/3");
         assertThat(invoice2.getInstallments().get(1).getDescription()).isEqualTo("amazon 2/2");
         final var invoice3 = invoices.get(2);
-        assertThat(invoice3.getClosingDate()).isEqualTo(LocalDate.parse("2023-11-25"));
-        assertThat(invoice3.getDueDate()).isEqualTo(LocalDate.parse("2023-12-01"));
         assertThat(invoice3.getTotal()).isEqualTo(new BigDecimal("50.00"));
         assertThat(invoice3.getInstallments()).isNotEmpty().hasSize(1);
         assertThat(invoice3.getInstallments().get(0).getDescription()).isEqualTo("alibaba 3/3");
@@ -105,7 +101,6 @@ class ProcessInvoiceServiceTest {
 
     @Test
     void shouldThrowExceptionWhenInvoiceDateIsNotOnRange() {
-        when(cardRepository.findById("cId")).thenReturn(Optional.of(Card.builder().dayOfDueDate(1).build()));
         when(invoiceRepository.findAllByCard_Id("cId")).thenReturn(Collections.emptyList());
 
         assertThatIllegalArgumentException()
@@ -115,12 +110,11 @@ class ProcessInvoiceServiceTest {
                         "alibaba",
                         LocalDateTime.parse("2023-02-01T20:41:37"),
                         "cId"))
-                .withMessage("'ocurredOn' must not be before current invoice closing date");
+                .withMessageContaining("'ocurredOn' must not be before 'current invoice' closing date - ");
     }
 
     @Test
     void shouldThrowExceptionWhenOcurrenceDateIsFuture() {
-        when(cardRepository.findById("cId")).thenReturn(Optional.of(Card.builder().dayOfDueDate(1).build()));
         when(invoiceRepository.findAllByCard_Id("cId")).thenReturn(Collections.emptyList());
 
         assertThatIllegalArgumentException()
