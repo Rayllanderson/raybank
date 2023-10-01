b90876a15c65e833337df3e791437c8ef79b1f11
Rayllanderson <rayllanderson@gmail.com>
2023-10-01 16:10:37 -0300

    criando plano de parcelamento ao realizar pagamento com cartao de credito

--- api/src/main/java/com/rayllanderson/raybank/card/services/payment/strategies/CreditCardPaymentStrategy.java
+++ api/src/main/java/com/rayllanderson/raybank/card/services/payment/strategies/CreditCardPaymentStrategy.java
@@ -1,10 +1,12 @@
 package com.rayllanderson.raybank.card.services.payment.strategies;
 
-import com.rayllanderson.raybank.card.events.CardPaymentCompletedEvent;
+import com.rayllanderson.raybank.card.events.CardCreditPaymentCompletedEvent;
 import com.rayllanderson.raybank.card.models.Card;
 import com.rayllanderson.raybank.card.services.payment.PaymentCardInput;
-import com.rayllanderson.raybank.card.transactions.payment.CardPaymentTransaction;
+import com.rayllanderson.raybank.card.transactions.payment.CardCreditPaymentTransaction;
 import com.rayllanderson.raybank.event.IntegrationEventPublisher;
+import com.rayllanderson.raybank.installment.services.create.CreateInstallmentPlanMapper;
+import com.rayllanderson.raybank.installment.services.create.CreateInstallmentPlanService;
 import com.rayllanderson.raybank.transaction.models.Transaction;
 import com.rayllanderson.raybank.transaction.repositories.TransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +19,21 @@ public class CreditCardPaymentStrategy implements CardPaymentStrategy {
 
     private final IntegrationEventPublisher eventPublisher;
     private final TransactionRepository transactionRepository;
+    private final CreateInstallmentPlanMapper planMapper;
+    private final CreateInstallmentPlanService createInstallmentPlanService;
 
     @Override
     @Transactional
     public Transaction pay(final PaymentCardInput payment, final Card card) {
+        final var transaction = transactionRepository.save(CardCreditPaymentTransaction.from(payment, card));
 
         card.pay(payment.toCreditCardPayment());
 
-        final var transaction = transactionRepository.save(CardPaymentTransaction.from(payment, card));
-        eventPublisher.publish(new CardPaymentCompletedEvent(transaction));
+        final var createPlanInput = planMapper.from(transaction);
+        final var installmentPlan = createInstallmentPlanService.create(createPlanInput);
+        transaction.addPlan(installmentPlan.getId());
+
+        eventPublisher.publish(new CardCreditPaymentCompletedEvent(transaction));
 
         return transaction;
     }
