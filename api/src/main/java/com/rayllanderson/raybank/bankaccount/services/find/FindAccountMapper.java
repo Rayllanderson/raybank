af4418a8836ef9e60964c77aa6c218874b9a9043
Rayllanderson <rayllanderson@gmail.com>
2023-10-22 15:50:55 -0300

    find account details

---
+++ api/src/main/java/com/rayllanderson/raybank/bankaccount/services/find/FindAccountMapper.java
@@ -0,0 +1,18 @@
+package com.rayllanderson.raybank.bankaccount.services.find;
+
+import com.rayllanderson.raybank.bankaccount.controllers.find.BankAccountDetailsResponse;
+import com.rayllanderson.raybank.bankaccount.controllers.transfer.BankAccountTransferResponse;
+import com.rayllanderson.raybank.bankaccount.model.BankAccount;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface FindAccountMapper {
+
+    @Mapping(target = "account", expression = "java(mapFrom(bankAccount))")
+    BankAccountDetailsOutput from(BankAccount bankAccount);
+
+    BankAccountDetailsOutput.AccountOutput mapFrom(BankAccount account);
+
+    BankAccountDetailsResponse from(BankAccountDetailsOutput output);
+}
