4f5b960b2e1d39b919bee93e136dbbf9a51f8fd1
Rayllanderson <rayllanderson@gmail.com>
2023-11-29 22:41:27 -0300

    criando card de saldo

--- v2/frontend/src/app/page.tsx
+++ v2/frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 
-import { Avatar, Dropdown, Navbar, Sidebar as FlowbiteSidebar, Card } from 'flowbite-react'
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
-import Link from 'next/link'
+
 import React from 'react'
 import { SidebarProvider } from './context/SidebarContext';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
+import { AcademicCapIcon } from '@heroicons/react/24/solid';
+import { FaMoneyBillTransfer, FaEyeSlash } from "react-icons/fa6";
+import { Card } from 'flowbite-react';
 
 export default function page() {
   return (
@@ -18,18 +19,39 @@ export default function page() {
 
         <main className="mx-4 mt-4 mb-24 w-full h-full order-2 flex justify-center">
 
-          <div className="cards flex w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl flex-col">
+          <div className="cards flex w-full max-w-sm md:max-w-md lg:max-w-lg flex-col">
             <div className="flex flex-col h-screen space-y-10">
-              <Card className="w-full p-10">
+              <Card className="p-3">
+                <div className="flex justify-between mt-3">
+                  <p className="text-lg font-mono font-semibold dark:text-white">Saldo Atual</p>
+                  <p className="text-lg font-mono font-bold text-pink-600 cursor-pointer">Ver extrato</p>
+                </div>
+                <div className='flex'>
+                  <p className="text-3xl font-mono font-bold dark:text-white">R$ 2.337.24</p>
+                  <div className='flex justify-center items-center ml-4 cursor-pointer'>
+                    <FaEyeSlash className='text-pink-600 w-7 h-7' />
+                  </div>
+                </div>
 
+                <div className='flex justify-between mt-5'>
+
+                  {["Transferir", "Pagar", "Depositar"].map((i) => {
+                    return <Card key={i} className="text-pink-600 bg-gray-100 hover:scale-105 transition-all p-1 cursor-pointer w-24 h-24 md:w-28 lg:w-32">
+                      <div className='flex justify-center items-center'>
+                        <FaMoneyBillTransfer className='w-6 h-6' />
+                      </div>
+                      <div className='flex justify-center'>
+                        <p className='text-sm md:text-md font-mono font-semibold'>{i}</p>
+                      </div>
+                    </Card>
+                  })}
+
+
+
+
+                </div>
               </Card>
 
-              <Card className="w-full p-10">
-
-              </Card>
-              <Card className="w-full p-10">
-
-              </Card>
             </div>
 
           </div>
