e16a04c5a7862662c7d3e3b58dbe998d54b63cc5
Rayllanderson <rayllanderson@gmail.com>
2023-12-11 20:12:25 -0300

    ajuste transition mini card

--- v2/frontend/src/app/components/cards/MiniCard.tsx
+++ v2/frontend/src/app/components/cards/MiniCard.tsx
@@ -9,7 +9,7 @@ export interface MiniCardProps {
 
 export function MiniCard({title, icon: Icon}: MiniCardProps) {
   return (
-    <Card className="text-primary-2 bg-gray-100 hover:scale-110 hover:text-primary-1 transition-all p-1 cursor-pointer w-24 h-24 md:w-28 lg:w-32">
+    <Card className="text-primary-2 bg-gray-100 hover:scale-110 hover:text-primary-1 transform transition-transform p-1 cursor-pointer w-24 h-24 md:w-28 lg:w-32">
       <div className='flex justify-center items-center'>
         <Icon className='w-6 h-6' />
       </div>
