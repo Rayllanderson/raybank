059b6877c447d67031ca76bee3847c8667cf6a5d
Rayllanderson <rayllanderson@gmail.com>
2024-01-21 14:20:26 -0300

    criando component para pix card group

---
+++ v2/frontend/src/app/pix/PixCardGroup.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { AjustLimitPixModal } from '@/components/AjustLimitPixModal';
+import { MiniCard } from '@/components/cards/MiniCard'
+import React, { useState } from 'react'
+import { FaKey, FaListUl, FaMoneyBill, FaMoneyBillTransfer, FaQrcode, FaSliders } from 'react-icons/fa6';
+
+
+export default function PixCardGroup() {
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+            <AjustLimitPixModal show={showModal} setOpenModal={setShowModal} />
+
+            <div className="space-y-5 mt-3">
+                <CardGroup>
+                    <MiniCard href='/transfer' icon={FaMoneyBillTransfer} title="Transferir" />
+                    <MiniCard href='/payments/pix' icon={FaQrcode} title="Pagar" />
+                    <MiniCard href='/deposits/pix' icon={FaMoneyBill} title="Depositar" />
+                </CardGroup>
+
+                <CardGroup>
+                    <MiniCard href='' icon={FaKey} title="Registrar Chave" />
+                    <MiniCard href='' icon={FaListUl} title="Listar Chaves" />
+                    <MiniCard onClick={() => setShowModal(true)} icon={FaSliders} title="Ajustar Limite" />
+                </CardGroup>
+            </div>
+        </>
+    )
+}
+
+
+function CardGroup({ children }: { children: any }) {
+    return <div className="flex space-x-4 ">
+        {children}
+    </div>
+}
--- v2/frontend/src/app/pix/page.tsx
+++ v2/frontend/src/app/pix/page.tsx
@@ -1,47 +1,23 @@
-'use client';
-import React, { useState } from 'react'
+import React from 'react'
 import { Container } from '../../components/Container';
-import { MiniCard } from '../../components/cards/MiniCard';
 import { Card } from '../../components/cards/Card';
-import { FaKey, FaListUl, FaMoneyBill, FaMoneyBillTransfer, FaPix, FaQrcode, FaSliders } from 'react-icons/fa6';
-import { AjustLimitPixModal } from '@/components/AjustLimitPixModal';
+import { FaPix } from 'react-icons/fa6';
+import PixCardGroup from './PixCardGroup';
 
 
 export default function page() {
-    const [showModal, setShowModal] = useState(false);
 
     return (
         <Container>
-            <AjustLimitPixModal show={showModal} setOpenModal={setShowModal}  />
             <Card >
                 <header className="flex items-center space-x-2">
                     <h1 className='text-xl md:text-2xl font-semibold font-mono'>Área Pix</h1>
                     <FaPix className='w-6 h-6 text-primary-2'/>
                 </header>
-
-                <div className="space-y-5 mt-3">
-                    <CardGroup>
-                        <MiniCard href='/transfer' icon={FaMoneyBillTransfer} title="Transferir" />
-                        <MiniCard href='/payments/pix' icon={FaQrcode} title="Pagar" />
-                        <MiniCard href='/deposits/pix' icon={FaMoneyBill} title="Depositar" />
-                    </CardGroup>
-
-                    <CardGroup>
-                        <MiniCard href='' icon={FaKey} title="Registrar Chave" />
-                        <MiniCard href='' icon={FaListUl} title="Listar Chaves" />
-                        <MiniCard onClick={() => setShowModal(true)} icon={FaSliders} title="Ajustar Limite" />
-                    </CardGroup>
-                </div>
-
+                <PixCardGroup />
             </Card>
         </Container>
     )
 
-}
-
-function CardGroup({ children }: {children: any}) {
-    return <div className="flex space-x-4 ">
-        {children}
-    </div>
 }
 
