95367685c6a69642e4f028a95ba7e784231211ae
Rayllanderson <rayllanderson@gmail.com>
2024-01-26 20:17:37 -0300

    ajustando side bar items

--- v2/frontend/src/components/SideBar.tsx
+++ v2/frontend/src/components/SideBar.tsx
@@ -23,46 +23,20 @@ export default function SideBar() {
   return (
     <Sidebar>
       <FlowbiteSidebar.Items>
-        <FlowbiteSidebar.ItemGroup>
-          
-        <Link href="/accounts">
-          <FlowbiteSidebar.Item icon={FaWallet}>
-            Conta
-          </FlowbiteSidebar.Item>
-        </Link>
+        <ul className="mt-4 space-y-2 border-t border-gray-200 pt-4 first:mt-0 first:border-t-0 first:pt-0 dark:border-gray-700">
+          <CustomSideBarItem icon={FaWallet} href="/accounts" title='Conta' />
 
-        <Link href="/cards">
-          <FlowbiteSidebar.Item icon={FaCcMastercard}>
-            Cartão de crédito
-          </FlowbiteSidebar.Item>
-          </Link>
+          <CustomSideBarItem icon={FaCcMastercard} href="/cards" title='Cartão de crédito' />
 
-          <Link href="/pix">
-            <FlowbiteSidebar.Item href="/pix" icon={FaPix}>
-              Pix
-            </FlowbiteSidebar.Item>
-          </Link>
+          <CustomSideBarItem icon={FaPix} href="/pix" title='Pix' />
 
-          <Link href="/boletos">
-            <FlowbiteSidebar.Item icon={FaBarcode}>
-              Boleto
-            </FlowbiteSidebar.Item>
-          </Link>
+          <CustomSideBarItem icon={FaBarcode} href="/boletos" title='Boleto' />
 
+          <CustomSideBarItem icon={FaMoneyBillTransfer} href="/transfer" title='Transferir' />
 
-          <Link href="/transfer">
-          <FlowbiteSidebar.Item icon={FaMoneyBillTransfer}>
-            Transferir
-          </FlowbiteSidebar.Item>
-          </Link>
+          <CustomSideBarItem icon={FaMoneyBillTrendUp} href="/deposits" title='Depositar' />
+        </ul>
 
-          <Link href="/deposits">
-          <FlowbiteSidebar.Item icon={FaMoneyBillTrendUp}>
-            Depositar
-          </FlowbiteSidebar.Item>
-          </Link>
-
-        </FlowbiteSidebar.ItemGroup>
         <FlowbiteSidebar.ItemGroup>
           <FlowbiteSidebar.Item href="#" icon={HiViewBoards}>
             Help
@@ -70,5 +44,16 @@ export default function SideBar() {
         </FlowbiteSidebar.ItemGroup>
       </FlowbiteSidebar.Items>
     </Sidebar>
+  )
+}
+
+function CustomSideBarItem({href, icon: Icon, title}: {href: string, icon: any, title: string}) {
+  return (
+    <li>
+      <Link href={href} className="flex items-center justify-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+        <Icon className="h-6 w-6 flex-shrink-0 text-gray-600 transition duration-75 group-hover:text-gray-900 dark:text-gray-300 dark:group-hover:text-white" />
+        <span className="px-3 flex-1 whitespace-nowrap">{title}</span>
+      </Link>
+    </li>
   )
 }
