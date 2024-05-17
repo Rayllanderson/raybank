e5d646953f304d1acb9ea2d827f2ec380fb0a394
Rayllanderson <rayllanderson@gmail.com>
2024-05-17 17:31:15 -0300

    ajustes messages not found

--- v2/frontend/src/app/accounts/not-found.tsx
+++ v2/frontend/src/app/accounts/not-found.tsx
@@ -4,8 +4,6 @@ export default function NotFound() {
   return (
     <div className="flex flex-col mt-5 text-center text">
       <div className="text-lg">Conta não encontrada ou ainda não foi criada. </div>
-      <div>Pode ser que ela ainda esteja em processo de criação. Por favor, tente novamente em alguns segundos.</div>
-      <div>Se o problema persistir, entre em contato conosco para obter assistência adicional. Agradecemos sua paciência!</div>
     </div>
   )
 }
