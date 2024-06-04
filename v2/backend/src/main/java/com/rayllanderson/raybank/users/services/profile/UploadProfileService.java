4c9361eb29f7d0e6e863a755d480bc7b351c28b1
Rayllanderson <rayllanderson@gmail.com>
2024-06-03 21:11:35 -0300

    upload profile service

---
+++ v2/backend/src/main/java/com/rayllanderson/raybank/users/services/profile/UploadProfileService.java
@@ -0,0 +1,40 @@
+package com.rayllanderson.raybank.users.services.profile;
+
+import com.rayllanderson.raybank.core.services.S3Service;
+import com.rayllanderson.raybank.core.services.S3UploadOutput;
+import com.rayllanderson.raybank.users.gateway.UserGateway;
+import com.rayllanderson.raybank.users.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class UploadProfileService {
+
+    private final S3Service s3Service;
+    private final UserGateway userGateway;
+
+    public S3UploadOutput upload(String userId, MultipartFile file) throws IOException {
+        final User user = userGateway.findById(userId);
+
+        if (user.hasProfilePicture()) {
+            deleteOldProfilePicture(user.getProfilePictureKey());
+        }
+
+        final S3UploadOutput s3UploadOutput = s3Service.uploadFile(file);
+
+        user.addProfilePicture(s3UploadOutput.toProfilePicture());
+        userGateway.save(user);
+
+        return s3UploadOutput;
+    }
+
+
+    private void deleteOldProfilePicture(final String oldProfilePictureKey) {
+        s3Service.deleteFile(oldProfilePictureKey);
+    }
+
+}
