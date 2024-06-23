f7a055550da37099c2d03a62c1f7e51a91551b5b
Rayllanderson <rayllanderson@gmail.com>
2024-06-23 11:43:45 -0300

    criando s3 notification e fila sqs para thumbnail

--- v2/infra/localstack/terraform/main.tf
+++ v2/infra/localstack/terraform/main.tf
@@ -19,10 +19,49 @@ resource "aws_sqs_queue" "keycloak_event_listener_queue" {
   visibility_timeout_seconds = 30
 }
 
+resource "aws_sqs_queue" "thumbnail_event_listener_queue" {
+  name                      = "thumbnail-event-listener-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 345600
+  visibility_timeout_seconds = 30
+}
+
+
 resource "aws_lambda_function" "create_thumbnail" {
   function_name = "CreateThumbnail"
   filename      = "${path.module}/../lambda/function.zip" 
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   role          = "arn:aws:iam::000000000000:role/lambda-role"
+}
+
+resource "aws_lambda_permission" "allow_s3_to_invoke_lambda" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.create_thumbnail.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raybank_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "upload_bucket_notification" {
+  bucket = aws_s3_bucket.raybank_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.create_thumbnail.arn
+    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+    filter_suffix       = "uploads/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_to_invoke_lambda]
+}
+
+resource "aws_s3_bucket_notification" "thumbnail_to_sqs_notification" {
+  bucket = aws_s3_bucket.raybank_bucket.id
+
+  queue {
+    queue_arn = aws_sqs_queue.thumbnail_event_listener_queue.arn
+    events    = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+    filter_prefix = "thumbnail/"
+  }
 }
