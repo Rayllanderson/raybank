1c40054fb7465570dbc46dbc6a68a67aac9b85c7
Rayllanderson <rayllanderson@gmail.com>
2024-07-13 11:10:17 -0300

    ajustando lambda function

--- v2/infra/localstack/lambda/lambda_function.py
+++ v2/infra/localstack/lambda/lambda_function.py
@@ -1,30 +1,42 @@
-import json
 import boto3
 from PIL import Image
 from io import BytesIO
 
-s3 = boto3.client('s3')
+s3_client = boto3.client('s3')
 
-def lambda_handler(event, context):
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        
-        response = s3.get_object(Bucket=bucket, Key=key)
-        image_data = response['Body'].read()
-        
-        image = Image.open(BytesIO(image_data))
-        
-        thumbnail_size = (128, 128)
-        image.thumbnail(thumbnail_size)
-        
-        buffer = BytesIO()
-        image.save(buffer, 'JPEG')
-        buffer.seek(0)
-        
-        thumbnail_key = f"thumbnails/{key}"
-        s3.put_object(Bucket=bucket, Key=thumbnail_key, Body=buffer, ContentType='image/jpeg')
-        
+def handler(event, context):
+    # Extrair informações do evento S3
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Baixar a imagem do S3
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    image_content = response['Body'].read()
+    
+    # Abrir a imagem com Pillow
+    image = Image.open(BytesIO(image_content))
+    
+    # Criar a thumbnail
+    thumbnail_size = (128, 128)
+    image.thumbnail(thumbnail_size)
+    
+    # Salvar a thumbnail em um objeto BytesIO
+    thumbnail_buffer = BytesIO()
+    image.save(thumbnail_buffer, 'JPEG')
+    thumbnail_buffer.seek(0)
+    
+    # Definir o novo nome do arquivo para a thumbnail
+    thumbnail_key = f"thumbnails/{key}"
+    
+    # Upload da thumbnail para o S3
+    s3_client.put_object(
+        Bucket=bucket,
+        Key=thumbnail_key,
+        Body=thumbnail_buffer,
+        ContentType='image/jpeg'
+    )
+    
     return {
-        'statusCode': 200
+        'statusCode': 200,
+        'body': f'Thumbnail created and uploaded to {thumbnail_key}'
     }
--- v2/infra/localstack/terraform/main.tf
+++ v2/infra/localstack/terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region                     = "us-east-1"
-  access_key                 = "test"
-  secret_key                 = "test"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
-  skip_requesting_account_id = true
-  skip_metadata_api_check    = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 }
 
 resource "aws_s3_bucket" "raybank_bucket" {
@@ -12,56 +12,52 @@ resource "aws_s3_bucket" "raybank_bucket" {
 }
 
 resource "aws_sqs_queue" "keycloak_event_listener_queue" {
-  name                      = "keycloak-event-listener-queue"
-  delay_seconds             = 0
-  max_message_size          = 262144
-  message_retention_seconds = 345600
+  name                       = "keycloak-event-listener-queue"
+  delay_seconds              = 0
+  max_message_size           = 262144
+  message_retention_seconds  = 345600
   visibility_timeout_seconds = 30
 }
 
 resource "aws_sqs_queue" "thumbnail_event_listener_queue" {
-  name                      = "thumbnail-event-listener-queue"
-  delay_seconds             = 0
-  max_message_size          = 262144
-  message_retention_seconds = 345600
+  name                       = "thumbnail-event-listener-queue"
+  delay_seconds              = 0
+  max_message_size           = 262144
+  message_retention_seconds  = 345600
   visibility_timeout_seconds = 30
 }
 
 
-resource "aws_lambda_function" "create_thumbnail" {
-  function_name = "CreateThumbnail"
-  filename      = "${path.module}/../lambda/function.zip" 
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "lambda_function"
+  filename      = "${path.module}/../lambda/my_deployment_package.zip"
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
+  runtime       = "python3.11"
   role          = "arn:aws:iam::000000000000:role/lambda-role"
 }
+
 
 resource "aws_lambda_permission" "allow_s3_to_invoke_lambda" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.create_thumbnail.function_name
+  function_name = aws_lambda_function.lambda_function.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.raybank_bucket.arn
-}
-
-resource "aws_s3_bucket_notification" "upload_bucket_notification" {
-  bucket = aws_s3_bucket.raybank_bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.create_thumbnail.arn
-    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
-    filter_suffix       = "uploads/"
-  }
-
-  depends_on = [aws_lambda_permission.allow_s3_to_invoke_lambda]
 }
 
 resource "aws_s3_bucket_notification" "thumbnail_to_sqs_notification" {
   bucket = aws_s3_bucket.raybank_bucket.id
 
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "uploads/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_to_invoke_lambda]
   queue {
-    queue_arn = aws_sqs_queue.thumbnail_event_listener_queue.arn
-    events    = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
-    filter_prefix = "thumbnail/"
+    queue_arn     = aws_sqs_queue.thumbnail_event_listener_queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "thumbnails/"
   }
 }
